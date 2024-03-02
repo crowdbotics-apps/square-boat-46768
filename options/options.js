d9c1f7c6b2861ad020dd6e850d9fcc10554c24a3
Crowdbotics <team@crowdbotics.com>
2024-03-02 19:47:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-boat-46768",
+  url: "https://square-boat-46768.botics.co",
+  api: "https://square-boat-46768.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
